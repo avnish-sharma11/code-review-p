4e048a5ad52e7b7cdd8ada9990e6dcd62de49fb8
avnish_sharma011 <127170456+avnish-sharma11@users.noreply.github.com>
2025-12-13 15:09:41 +0530

    Update app.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -4,7 +4,11 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: "https://code-rev.onrender.com",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}))
 
 
 app.use(express.json())
